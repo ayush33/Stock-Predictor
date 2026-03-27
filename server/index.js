eec4b6f51570f024d37b885c9066cb7edf8e27d1
ayush33 <ayush.1996.jain@gmail.com>
2026-03-27 16:49:45 +0530

    fix code

--- server/index.js
+++ server/index.js
@@ -16,24 +16,29 @@ app.use(express.json());
 console.log('aa',process.env.ANTHROPIC_API_KEY,)
 console.log("API KEY LOADED:", process.env.ANTHROPIC_API_KEY?.slice(0, 15));
 app.post("/api/analyze", async (req, res) => {
-    console.log("Anthropic response received1");
-
-  try {
-    const response = await fetch("https://api.anthropic.com/v1/messages", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key": process.env.ANTHROPIC_API_KEY,
-        "anthropic-version": "2023-06-01",
-      },
-      body: JSON.stringify(req.body),
-    });
-    console.log("Anthropic response received");
-    const data = await response.json();
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
+    console.log("Request reached backend");
+  
+    try {
+      console.log("Calling Anthropic API");
+  
+      const response = await fetch("https://api.anthropic.com/v1/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": process.env.ANTHROPIC_API_KEY,
+          "anthropic-version": "2023-06-01",
+        },
+        body: JSON.stringify(req.body),
+      });
+  
+      console.log("Anthropic response received");
+  
+      const data = await response.json();
+      res.json(data);
+  
+    } catch (err) {
+      console.log("Error:", err);
+      res.status(500).json({ error: err.message });
+    }
+  });
 app.listen(3001, () => console.log("✅ Server running on http://localhost:3001"));
